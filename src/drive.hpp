0a6133ccb3388520803f88060295e5abbb5843e8
Daniel <daniel.alp.email@gmail.com>
2024-04-14 17:22:05 -0400

    Measured velocity and acceleration constraints

--- src/drive.hpp
+++ src/drive.hpp
@@ -2,8 +2,8 @@
 #include "main.h"
 
 //Need to be measured and tuned. 
-const double MAX_VELOCITY = 10; // in/s
-const double MAX_ACCELERATION = 40; // in/s^2
+const double MAX_VELOCITY = 75; // in/s (a max velocity of 79.26in/s was measured but we are giving a small buffer)
+const double MAX_ACCELERATION = 0.10; // in/s^2 (an acceleration of 0.12in/s and 0.18in/s was measured)
 
 const double DRIVE_WIDTH = 15; // in (distance between where middle wheels touch the floor). Needs to be measured.
 const double WHEEL_DIAM = 3.25; // in
