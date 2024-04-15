71d5338f7883db5fff83d4b73aa6f7611abed8fc
Daniel <daniel.alp.email@gmail.com>
2024-04-15 14:38:26 -0400

    Added rate limit and bounds to power for turn PID (untested)

--- src/pid.cpp
+++ src/pid.cpp
@@ -12,23 +12,27 @@ void turn_to_heading(double heading_goal) {
     double kP = 0;
     double kD = 0;
     double min_power = 0;
+    double MAX_POWER_CHANGE = 12000;
 
     double error = 0;
     double error_prev = 0;
 
     double power = 0;
+    double power_prev = 0;
 
     do {
         error = get_heading_difference(imu.get_heading(), heading_goal);
 
         //Technically 12000 and LOOP_DELAY_SEC can be baked into the kP and kD terms
-        //But I'm hoping we can reuse the values tuned for straight movements
         power = (kP * error + kD * (error - error_prev) / LOOP_DELAY_SEC  + min_power) * 12000;
+        power = std::clamp(power, power - MAX_POWER_CHANGE, power + MAX_POWER_CHANGE);
+        power = std::clamp(power, static_cast<double>(-12000), static_cast<double>(12000));
 
         move_voltage_right_drive(-power);
         move_voltage_left_drive(power);
 
         error_prev = error;
+        power_prev = power;
 
         delay(LOOP_DELAY_MS);
     } while (std::abs(error) > error_threshold || get_left_velocity() > 5 || get_right_velocity() > 5);
