7e6e171536b5bbc8dbc7980ebf53afffe63e2fdf
Daniel <daniel.alp.email@gmail.com>
2024-04-16 14:47:46 -0400

    removed acceleration limiter from turn pid

--- src/pid.cpp
+++ src/pid.cpp
@@ -10,12 +10,10 @@ void turn_to_heading(double heading_goal) {
 
     //NEED TO BE TUNED
     const double MAX_VELOCITY_ANGULAR = 0.500;//Deg/ms
-    const double MAX_ACCELERATION_ANGULAR = 1;//Deg/ms^2
 
     const double kP = 0.080;
     const double kD = 0.0085;
-    const double MIN_POWER = 0.000;
-    const double MAX_POWER_CHANGE = 12000 * (MAX_ACCELERATION_ANGULAR * LOOP_DELAY_MS);
+    const double MIN_POWER = 0.01;
 
     double abs_start_heading_diff = std::abs(get_heading_difference(imu.get_heading(), heading_goal));
 
@@ -37,8 +35,6 @@ void turn_to_heading(double heading_goal) {
 
         //Technically 12000 and LOOP_DELAY_SEC can be baked into the kP and kD terms
         power = (kP * error + kD * (error - error_prev) / LOOP_DELAY_SEC  + MIN_POWER) * 12000;
-        power = std::clamp(power, power_prev - MAX_POWER_CHANGE, power_prev + MAX_POWER_CHANGE);
-        power = std::clamp(power, static_cast<double>(-12000), static_cast<double>(12000));
 
         move_voltage_right_drive(-power);
         move_voltage_left_drive(power);
