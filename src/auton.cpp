3a452087a9dda4548baf86d58ec4399891b4b06e
Daniel <daniel.alp.email@gmail.com>
2024-04-16 14:43:58 -0400

    Tuned turn constants

--- src/auton.cpp
+++ src/auton.cpp
@@ -7,20 +7,6 @@
 #include "pid.hpp"
 
 void test_auton() {
-    FILE* log_file = fopen("/usd/motion-profile-data.txt", "w");
-    fprintf(log_file, "Time, Heading\n");
-
-    double time_elapsed_ms = 0;
-    while (time_elapsed_ms < 5000) {
-        move_voltage_left_drive(12000);
-        move_voltage_right_drive(-12000);
-        pros::delay(10);
-        time_elapsed_ms += 10;
-
-        fprintf(log_file, "%f, %f\n", time_elapsed_ms, imu.get_heading());
-    }
-    fclose(log_file);
-
     // //Specify is reversed or not, default is not reversed.
     // move_straight(72, 0, 0, 0);
     // move_straight(72, 0, 0, 0, true);
@@ -28,15 +14,12 @@ void test_auton() {
     // move_straight(24, 0, 0, 0);
     // move_straight(48, 0, 0, 0, true);
 
-    // turn_to_heading(90);
+    turn_to_heading(90);
+    pros::delay(500);
+    turn_to_heading(0);
+    pros::delay(500);
+    turn_to_heading(270);
     // pros::delay(200);
     // turn_to_heading(0);
     // pros::delay(200);
-    // turn_to_heading(270);
-    // pros::delay(200);
-    // turn_to_heading(0);
-    // pros::delay(200);
-    // turn_to_heading(45);
-    // turn_to_heading(45);
-    // turn_to_heading(135);
 }
--- src/pid.cpp
+++ src/pid.cpp
@@ -6,13 +6,18 @@
 #include "mathutil.hpp"
 
 void turn_to_heading(double heading_goal) {
-    const double ERROR_THRESHOLD = 2;
+    const double ERROR_THRESHOLD = 1;
 
     //NEED TO BE TUNED
-    const double kP = 0.05;
-    const double kD = 0.0052;
-    const double MIN_POWER = 0.01;
-    const double MAX_POWER_CHANGE = 12000;
+    const double MAX_VELOCITY_ANGULAR = 0.500;//Deg/ms
+    const double MAX_ACCELERATION_ANGULAR = 1;//Deg/ms^2
+
+    const double kP = 0.080;
+    const double kD = 0.0085;
+    const double MIN_POWER = 0.000;
+    const double MAX_POWER_CHANGE = 12000 * (MAX_ACCELERATION_ANGULAR * LOOP_DELAY_MS);
+
+    double abs_start_heading_diff = std::abs(get_heading_difference(imu.get_heading(), heading_goal));
 
     double error = 0;
     double error_prev = 0;
@@ -32,7 +37,7 @@ void turn_to_heading(double heading_goal) {
 
         //Technically 12000 and LOOP_DELAY_SEC can be baked into the kP and kD terms
         power = (kP * error + kD * (error - error_prev) / LOOP_DELAY_SEC  + MIN_POWER) * 12000;
-        power = std::clamp(power, power - MAX_POWER_CHANGE, power + MAX_POWER_CHANGE);
+        power = std::clamp(power, power_prev - MAX_POWER_CHANGE, power_prev + MAX_POWER_CHANGE);
         power = std::clamp(power, static_cast<double>(-12000), static_cast<double>(12000));
 
         move_voltage_right_drive(-power);
@@ -44,14 +49,18 @@ void turn_to_heading(double heading_goal) {
         delay(LOOP_DELAY_MS);
         time_elapsed_ms += LOOP_DELAY_MS;
 
+        if (time_elapsed_ms > abs_start_heading_diff / MAX_VELOCITY_ANGULAR + 1000) {
+            break;
+        }
+
         fprintf(log_file, "%f, %f, %f\n",
             time_elapsed_ms, 
             heading_goal, 
             heading);
 
     } while (std::abs(error) > ERROR_THRESHOLD 
-            || get_left_velocity() > 2 
-            || get_right_velocity() > 2);
+            || get_left_velocity() > 6 
+            || get_right_velocity() > 6);
 
     move_voltage_left_drive(0);
     move_voltage_right_drive(0);
